06dd5ad9dbd82e2b596667a792edc078265cfafb
jorge zafra <jorge.zaframontero@gmail.com>
2021-09-30 10:21:28 +0200

    actualizacion

---
+++ src/README.md
@@ -0,0 +1,4 @@
+## prueba
+# Jorge Zafra Montero
+
+repositorio de prueba mongoDB. primeros pasos con find, deleteMany y el comando load de mongosh
---
+++ src/ejenplo01.js
@@ -0,0 +1,3 @@
+db.col01.insertOne( { nombre: "Pepe", edad: 19 } )
+db.col01.insertOne( { nombre: "Antonio", edad: 18 } )
+db.col01.insertOne( { nombre: "Jorge", edad: 19 } )
